1e1741fd612f4b8a43d506adab2b1e47ee7bf3a7
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-08-21 10:33:20 +0600

    added Language in admin.py

--- translate/admin.py
+++ translate/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from translate.models import Hashtag, LatestUpdate, OurOffer, Service, Industry, Review, FAQ
+from translate.models import Hashtag, LatestUpdate, OurOffer, Service, Industry, Review, FAQ, Language
 
 # Register your models here.
 
@@ -8,6 +8,7 @@ admin.site.register(Hashtag)
 admin.site.register(LatestUpdate)
 admin.site.register(OurOffer)
 admin.site.register(Service)
+admin.site.register(Language)
 admin.site.register(Industry)
 admin.site.register(Review)
 admin.site.register(FAQ)
