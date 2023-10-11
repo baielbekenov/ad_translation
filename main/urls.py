2fc9f1466ed4bd8c0b310cd151afdc4ac97df984
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-10-11 13:13:26 +0600

    modified settings and urls

--- main/urls.py
+++ main/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('translate.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken'))
