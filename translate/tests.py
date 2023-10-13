aa3d1eac9552306374c1452edbf52101d6f13644
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-10-13 12:15:49 +0600

    some tests

--- translate/tests.py
+++ translate/tests.py
@@ -1,8 +1,26 @@
 from django.test import TestCase
 
-from .models import Language
+from .models import Language, DetailLanguage, LatestUpdate, OurOffer
 
 class LanguageTest(TestCase):
     def test_string_representation(self):
         language = Language(name="English")
         self.assertEqual(str(language), language.name)
+        
+        
+# class DetailLanguageTest(TestCase):
+#     def test_string_representation(self):
+#         detaillanguage = DetailLanguage(id=1)
+#         self.assertEqual(str(detaillanguage), detaillanguage.name)
+        
+        
+class LatestUpdateTest(TestCase):
+    def test_string_representation(self):
+        latestupdate = LatestUpdate(title="Black spider")
+        self.assertEqual(str(latestupdate), latestupdate.title)
+        
+        
+class OurOfferTest(TestCase):
+    def test_string_representation(self):
+        ouroffer = OurOffer(title="Ouroffer")
+        self.assertEqual(str(ouroffer), ouroffer.title)
